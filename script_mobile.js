f78c920b7ac8bb6076150f88731065a6fe3d1a2f
Serena <serena@rumbleauto.com.au>
2026-05-12 10:42:56 +1000

    Add video and full truck images to mobile models

--- script_mobile.js
+++ script_mobile.js
@@ -139,7 +139,7 @@ function openVideoModal(){
   modal.classList.add("is-open");
   modal.setAttribute("aria-hidden","false");
   document.body.style.overflow = "hidden";
-  if(video){
+  if(video && typeof video.play === "function"){
     video.currentTime = 0;
     video.play().catch(() => {});
   }
@@ -148,8 +148,14 @@ function openVideoModal(){
 function resetVideoModal(){
   const video = document.querySelector(".video-modal-player");
   if(!video) return;
-  video.pause();
-  video.currentTime = 0;
+  if(typeof video.pause === "function"){
+    video.pause();
+    video.currentTime = 0;
+    return;
+  }
+  if(video.tagName === "IFRAME"){
+    video.src = video.src;
+  }
 }
 
 function closeModals(){
